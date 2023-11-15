0901c375c1bfea726361905a10e9f0d201881fdd
sanch13 <sanch13sanch@gmail.com>
2023-11-15 20:39:31 +0300

    added form_search

--- miran/books/forms.py
+++ miran/books/forms.py
@@ -2,9 +2,12 @@ from django import forms
 from .models import Book
 
 
-class BookSearchForm(forms.Form):
-    search = forms.CharField(max_length=100,
-                             required=False)
+class BookSearchForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('author', 'title', 'status', 'reader')
+    # search = forms.CharField(max_length=100,
+    #                          required=False)
 
 
 class AddBookForm(forms.ModelForm):
